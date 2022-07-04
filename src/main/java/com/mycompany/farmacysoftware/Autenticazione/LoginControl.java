ab9e0c179bb4f25215c6c824c024505bd91d5bfe
Kengi <manfri.crapa@gmail.com>
2022-07-04 19:53:33 +0200

    FarmacySoftware

---
+++ src/main/java/com/mycompany/farmacysoftware/Autenticazione/LoginControl.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.farmacysoftware.Autenticazione;
+
+public class LoginControl {
+    private String username;
+    private String password;
+    
+    public LoginControl(String username, String password){
+        this.username= username;
+        this.password=password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void ControlloNelDBMS(){
+        
+    }
+    public void RecuperoPassword(){
+        
+    }
+}

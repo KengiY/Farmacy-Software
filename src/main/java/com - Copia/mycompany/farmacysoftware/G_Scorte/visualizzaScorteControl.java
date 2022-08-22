3c7d8ff56807520e1ceaf2a16f29434809f84edf
Bruno-Feraz <79698951+Bruno-Feraz@users.noreply.github.com>
2022-08-22 19:16:51 +0200

    better
    molto meglio adesso, funziona sicuro me fra sereno

---
+++ src/main/java/com - Copia/mycompany/farmacysoftware/G_Scorte/visualizzaScorteControl.java
@@ -0,0 +1,14 @@
+package com.mycompany.farmacysoftware.G_Scorte;
+
+import java.util.LinkedList;
+
+
+public class visualizzaScorteControl {
+    private LinkedList<Farmaco> listafarmaci = new LinkedList<Farmaco>();
+    private Farmaco farmaco;
+
+    public void CollegamentoDBMS(){
+    
+    }
+}
+
